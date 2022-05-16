cc2774629d4c9040776be9539a98aa9f351afb95
Luca Andaloro <luca.andaloro4@gmail.com>
2022-05-16 16:13:30 +0200

    error e message

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -14,52 +14,66 @@
                 <div class="form-group">
                     <label for="title">Title</label>
                     <input type="text" class="form-control {{ $errors->has('title')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="title" placeholder="Insert the title here..." name="title" value="{{ count($errors) ? old('title') : $comic->title }}">
-                    <div id="titleFeedback" class="invalid-feedback">
-                        Please enter a valid title( Max 255 characters ).
-                    </div>
+                    @error('title')
+                        <div id="titleFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="thumb">Thumb</label>
                     <input type="text" class="form-control {{ $errors->has('thumb')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" placeholder="Insert the thumb here..." name="thumb" id="thumb" placeholder="Insert the thumb here..." name="thumb" value="{{ count($errors) ? old('thumb') : $comic->thumb }}">
-                    <div id="thumbFeedback" class="invalid-feedback">
-                        Please enter a valid URL.
-                    </div>
+                    @error('thumb')
+                        <div id="thumbFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="price">Price</label>
                     <input type="number" class="form-control {{ $errors->has('price')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="price" placeholder="Insert the price here..." name="price" value="{{ count($errors) ? old('price') : $comic->price }}">
-                    <div id="priceFeedback" class="invalid-feedback">
-                        Please enter a valid price.
-                    </div>
+                    @error('number')
+                        <div id="priceFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="series">Series</label>
                     <input type="text" class="form-control {{ $errors->has('series')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="series" placeholder="Insert the series here..." name="series" value="{{ count($errors) ? old('series') : $comic->series }}">
-                    <div id="seriesFeedback" class="invalid-feedback">
-                        Please enter a valid serie( Max 255 characters ).
-                    </div>
+                    @error('series')
+                        <div id="seriesFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="sale_date">Sale date</label>
                     <input type="date" class="form-control {{ $errors->has('sale_date')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="sale_date" placeholder="Insert the sale date here..." name="sale_date" value="{{ count($errors) ? old('sale_date') : $comic->sale_date }}">
-                    <div id="sale_dateFeedback" class="invalid-feedback">
-                        Please enter a valid sale_date.
-                    </div>
+                    @error('sale_date')
+                        <div id="sale_dateFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="type">Type</label>
                     <input type="text" class="form-control {{ $errors->has('type')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="type" placeholder="Insert the type here..." name="type" value="{{ count($errors) ? old('type') : $comic->type }}">
-                    <div id="typeFeedback" class="invalid-feedback">
-                        Please enter a valid type.
-                    </div>
+                    @error('type')
+                        <div id="typeFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
-                  <label for="description">Description:</label>
-                  <textarea class="form-control {{ $errors->has('description')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="description" rows="3" name="description" placeholder="Insert the description here...">{{ count($errors) ? old('description') : $comic->description }}</textarea>
-                  <div id="descriptionFeedback" class="invalid-feedback">
-                    Please enter a valid description.
-                </div>
+                    <label for="description">Description:</label>
+                    <textarea class="form-control {{ $errors->has('description')? 'is-invalid' : (count($errors) ? 'is-valid' : '') }}" id="description" rows="3" name="description" placeholder="Insert the description here...">{{ count($errors) ? old('description') : $comic->description }}</textarea>
+                    @error('description')
+                        <div id="descriptionFeedback" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <button type="submit" class="btn btn-primary">Update</button>
@@ -72,23 +86,23 @@
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
-                    <form onclick="deleteModal()" method="POST" action="{{route('comics.destroy', $comic)}}">
-                        @csrf
-                        @method('DELETE')
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">DELETING COMIC</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">DELETING COMIC</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <h3 class="text-center">Are you sure?</h3>
-                        </div>
-                        <div class="modal-footer">
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h3 class="text-center">Are you sure?</h3>
+                    </div>
+                    <div class="modal-footer">
+                        <form method="POST" action="{{route('comics.destroy', $comic)}}">
+                            @csrf
+                            @method('DELETE')
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
                             <button type="submit" class="btn btn-danger">DELETE</button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
               </div>
         </div>
